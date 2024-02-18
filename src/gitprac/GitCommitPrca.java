04444ed9d4966cb2998386a752b6c4bbe73f798a
sandyapolnati <sandya.polnati@gmail.com>
2024-02-18 12:34:53 +0530

    test

--- src/gitprac/GitCommitPrca.java
+++ src/gitprac/GitCommitPrca.java
@@ -4,7 +4,7 @@ public class GitCommitPrca {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Git prca hllo");
+		System.out.println("Git prca hllo"); 
 		//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
 	}
 }
